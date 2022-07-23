07df598eeac5e560477527e3a10f46e465ca0b8e
Yifei <workeryifei@outlook.com>
2022-07-23 18:51:00 +0800

    support query info of specific ip

--- cmd/ip.go
+++ cmd/ip.go
@@ -12,10 +12,20 @@ import (
 var ipCmd = &cobra.Command{
 	Use:   "ip",
 	Short: "Query the information of your ip or the specific ip. ",
-	Long:  `Query the information of your ip or the specific ip.`,
+	Long:  `Query the information of your ip or the specific ip.
+For example:
+	devbox ip           query the ip info of your host.
+
+	devbox ip 8.8.8.8   query the specific ip info.
+`,
 	Run: func(cmd *cobra.Command, args []string) {
-		err := queryMyPublicIPv4Info()
-		if err != nil {
+		var ip string
+		if len(args) == 1 {
+			ip = args[0]
+		} else if len(args) > 1 {
+			fmt.Println("error args")
+		}
+		if err := printIPv4Info(ip); err != nil {
 			fmt.Println(err)
 		}
 	},
@@ -25,8 +35,14 @@ func init() {
 	rootCmd.AddCommand(ipCmd)
 }
 
-func queryMyPublicIPv4Info() error {
-	rsp, err := http.Get("https://ipinfo.io/json")
+func printIPv4Info(ip string) error {
+	var url string
+	if ip == "" {
+		url = "https://ipinfo.io"
+	} else {
+		url = "https://ipinfo.io" + "/" + ip
+	}
+	rsp, err := http.Get(url)
 	if err != nil {
 		return err
 	}
@@ -38,5 +54,6 @@ func queryMyPublicIPv4Info() error {
 	}
 
 	fmt.Println(string(body))
+
 	return nil
 }
